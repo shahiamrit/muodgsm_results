bb6ee5158196d325114937d6c6b2d340a3125af7
amrit <ca@shahiamrit365.com.np>
2023-01-31 10:55:53 +0545

    added

--- NEConverter/NEC/form4.py
+++ NEConverter/NEC/form4.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class ResultForm(forms.Form):
-    symb = forms.CharField(label="", help_text="", widget=forms.TextInput(attrs={'placeholder': 'Enter Symbol#'}))
-    dob = forms.CharField(label="", help_text="", widget=forms.TextInput(attrs={'placeholder': 'Enter Date of Birth#'}))
+    symb = forms.CharField(label="", help_text="", widget=forms.TextInput(attrs={'placeholder': 'Symbol No.'}))
+    dob = forms.CharField(label="", help_text="", widget=forms.TextInput(attrs={'placeholder': 'DOB: YYYY-MM-DD'}))
