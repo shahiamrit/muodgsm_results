45b12a74efb314171bdc1e3662ec3c2a37892345
amrit <ca@shahiamrit365.com.np>
2023-01-31 16:00:02 +0545

    addded

--- NEConverter/NEC/forms3.py
+++ NEConverter/NEC/forms3.py
@@ -5,7 +5,7 @@ from .models import userLogin
 class ProductForm(ModelForm):
     class Meta:
         model = userLogin
-        fields = ['studentname', 'batch', 'subject', 'credithour', 'grade', 'gradevalu', 'lcgpa', 'semlettergrade', 'remarks', 'dob', 'symb', 'result', 'subject1', 'credithour1', 'grade1', 'gradevalu1', 'semlettergrade1', 'remarks1', 'subject2', 'credithour2', 'grade2', 'gradevalu2', 'semlettergrade2', 'remarks2', 'subject3', 'credithour3', 'grade3', 'gradevalu3', 'semlettergrade3', 'remarks3']
+        fields = ['studentname', 'batch', 'subject', 'credithour', 'grade', 'gradevalu', 'lcgpa', 'semlettergrade', 'remarks', 'dob', 'symb', 'reg', 'result', 'subject1', 'credithour1', 'grade1', 'gradevalu1', 'remarks1', 'subject2', 'credithour2', 'grade2', 'gradevalu2', 'remarks2', 'subject3', 'credithour3', 'grade3', 'gradevalu3', 'remarks3']
 
 
 class UploadForm(Form):
